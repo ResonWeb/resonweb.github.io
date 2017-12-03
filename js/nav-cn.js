7041d66feff1f7edaaa26657597f2bb6305f6098
resonhou <resonhou@163.com>
2017-12-04 00:51:08 +0800

    update nav-cn.js

--- js/nav-cn.js
+++ js/nav-cn.js
@@ -7,7 +7,7 @@ document.writeln("    		</ul>");*/
 document.writeln("    		<ul>");
 document.writeln("        		<li><a href=\"index-cn.html\">站点首页</a></li>");
 document.writeln("        		<li><a href=\"http://blog.sina.com.cn/u/3210098304\">生活动态</a></li>");
-document.writeln("        		<li><a href=\"https://reson-hou.github.io/blog\">个人博客</a></li>");
+document.writeln("        		<li><a href=\"https://www.reson.eu.org/blog\">个人博客</a></li>");
 document.writeln("        		<li><a href=\"contact.html\">联系本人</a></li>");
 document.writeln("        		<li><a href=\"favorite.html\">爱好链接</a></li>");
 document.writeln("    		</ul>");
