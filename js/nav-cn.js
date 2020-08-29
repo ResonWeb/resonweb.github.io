e6d5941b76cb05bd8ec1382cb340c5d95b6bd38f
ResonHou <resonhou@163.com>
2020-08-29 16:18:37 +0800

    修改博客地址到blog.reson.eu.org

--- js/nav-cn.js
+++ js/nav-cn.js
@@ -7,7 +7,7 @@ document.writeln("    		</ul>");*/
 document.writeln("    		<ul>");
 document.writeln("        		<li><a href=\"index-cn.html\">站点首页</a></li>");
 document.writeln("        		<li><a href=\"http://blog.sina.com.cn/u/3210098304\">生活动态</a></li>");
-document.writeln("        		<li><a href=\"https://resonhou.gitee.io/blog\">个人博客</a></li>");
+document.writeln("        		<li><a href=\"https://blog.reson.eu.org\">个人博客</a></li>");
 document.writeln("        		<li><a href=\"contact.html\">联系本人</a></li>");
 document.writeln("        		<li><a href=\"favorite.html\">爱好链接</a></li>");
 document.writeln("    		</ul>");
